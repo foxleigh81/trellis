217178e194d2f8749b15bff1829da49fefe599ce
Alexander Foxleigh <alex@alexfoxleigh.com>
2019-01-10 23:33:54 +0000

    added a TODO

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Trello = require('trello')
 const chalk = require('chalk')
 
 const trello = new Trello(process.env.API_KEY, process.env.APP_TOKEN)
-
+// TODO: Add ability to specify user (by user name) and to select which boards and lists to return
 trello.getBoards('535286eaaebdc2101ba65b2a', function(err, boards) {
   boards.forEach(board => {
     if (!board.closed) {
