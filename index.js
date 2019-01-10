c4a52e5ed105ad5f0091dc337402dcec1a8f31ab
Alexander Foxleigh <alex@alexfoxleigh.com>
2019-01-10 23:33:14 +0000

    empty boards no longer appear in list

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ const chalk = require('chalk')
 
 const trello = new Trello(process.env.API_KEY, process.env.APP_TOKEN)
 
-// TODO: Prevent empty lists from rendering
 trello.getBoards('535286eaaebdc2101ba65b2a', function(err, boards) {
   boards.forEach(board => {
     if (!board.closed) {
       trello.getListsOnBoard(board.id, function(err, lists) {
         lists.forEach((list) => {
           trello.getCardsOnList(list.id, function(err, cards) {
-            if (list.name === 'Todo') {
+            if (list.name === 'Todo' && cards.length > 0) {
               console.group(chalk.red(list.name), 'on', chalk.blue(board.name))
               cards.map(card => {
                 console.log(card.name)
