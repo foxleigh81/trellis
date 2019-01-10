ee67050700d6fc47d5916397722a614c3d69db0a
Alexander Foxleigh <alex@alexfoxleigh.com>
2019-01-10 23:02:21 +0000

    app now looks for a 'todo' list on every board and displays the cards

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ const chalk = require('chalk')
 
 const trello = new Trello(process.env.API_KEY, process.env.APP_TOKEN)
 
-trello.getListsOnBoard('8kYSHWCy', function(err, lists) {
-  // TODO: Make list sorted by 'pos'
-  lists.forEach(list => {
-    trello.getCardsOnList(list.id, function(err, cards) {
-      if (list.name !== 'Done' && list.name !== 'Inbox') {
-        console.group(chalk.red(list.name))
-        cards.map(card => {
-          console.log(card.name)
+// TODO: Prevent empty lists from rendering
+trello.getBoards('535286eaaebdc2101ba65b2a', function(err, boards) {
+  boards.forEach(board => {
+    if (!board.closed) {
+      trello.getListsOnBoard(board.id, function(err, lists) {
+        lists.forEach((list) => {
+          trello.getCardsOnList(list.id, function(err, cards) {
+            if (list.name === 'Todo') {
+              console.group(chalk.red(list.name), 'on', chalk.blue(board.name))
+              cards.map(card => {
+                console.log(card.name)
+              })
+              console.groupEnd(chalk.red(list.name))
+            }
+          })
         })
-        console.groupEnd(chalk.red(list.name))
-      }
-    })
+      })
+    }
   })
 })
