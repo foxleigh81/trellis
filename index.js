7075406021dca69f6b82396b1d7ff6655319bf1a
Alexander Foxleigh <alex@alexfoxleigh.com>
2019-01-11 00:27:33 +0000

    member id no longer required as data comes from auth token and multiple lists can now be specified in an array

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const chalk = require('chalk')
 const terminalLink = require('terminal-link')
 
 const trello = new Trello(process.env.API_KEY, process.env.APP_TOKEN)
-// TODO: Add ability to specify user (by user name) and to select which boards and lists to return
-trello.getBoards('535286eaaebdc2101ba65b2a', function(err, boards) {
+const listNameArray = ['Todo']
+trello.makeRequest('get', '/1/members/me/boards', {}, function(err, boards) {
   boards.forEach(board => {
     if (!board.closed) {
       trello.getListsOnBoard(board.id, function(err, lists) {
         lists.forEach((list) => {
           trello.getCardsOnList(list.id, function(err, cards) {
-            if (list.name === 'Todo' && cards.length > 0) {
+            // Only return the lists we are interested in and skip over lists with no cards
+            if ( listNameArray.includes(list.name) && cards.length > 0) {
               const boardLink = terminalLink(board.name, board.shortUrl)
               console.log(' ')
               console.log(`${chalk.bgGreen.black.bold(' ✔ ')}${chalk.bgWhite(` ${chalk.red(list.name)} ${chalk.black(`on`)} ${chalk.blue(boardLink)} `)}`)
