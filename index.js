d312ef983de59dbc9ee472137cbe53501eeaec7f
Alexander Foxleigh <alex@alexfoxleigh.com>
2019-01-11 00:16:33 +0000

    added board and card links plus updated formatting

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 
 const Trello = require('trello')
 const chalk = require('chalk')
+const terminalLink = require('terminal-link')
 
 const trello = new Trello(process.env.API_KEY, process.env.APP_TOKEN)
 // TODO: Add ability to specify user (by user name) and to select which boards and lists to return
@@ -14,11 +15,14 @@ trello.getBoards('535286eaaebdc2101ba65b2a', function(err, boards) {
         lists.forEach((list) => {
           trello.getCardsOnList(list.id, function(err, cards) {
             if (list.name === 'Todo' && cards.length > 0) {
-              console.group(chalk.red(list.name), 'on', chalk.blue(board.name))
+              const boardLink = terminalLink(board.name, board.shortUrl)
+              console.log(' ')
+              console.log(`${chalk.bgGreen.black.bold(' ✔ ')}${chalk.bgWhite(` ${chalk.red(list.name)} ${chalk.black(`on`)} ${chalk.blue(boardLink)} `)}`)
+              console.log(' ')
               cards.map(card => {
-                console.log(card.name)
+                const cardLink = terminalLink(card.name, card.shortUrl)
+                console.log(' ', cardLink)
               })
-              console.groupEnd(chalk.red(list.name))
             }
           })
         })
