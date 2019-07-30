a0a2edd57210730d6e1614071ce211bf722bd724
foxleigh81 <alex@alexfoxleigh.com>
2019-07-30 19:57:47 +0100

    modified file structure and minor refactoring

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,7 @@ const terminalLink = require('terminal-link')
 
 // TODO: Add error state if token is rejected
 
-const configGen = require("./configGen")
+const configGen = require("../lib/configGen")
 
 var configDir = path.resolve(configGen.homePath(), ".trellis");
 var configPath = path.resolve(configDir, "config.json");
@@ -29,21 +29,23 @@ const trello = new Trello(configData.appKey, configData.appToken)
 const listNameArray = (argv.list) ? [argv.list] : configData.getLists
 
 trello.makeRequest('get', '/1/members/me/boards', {}, function(err, boards) {
+  if (err) throw new Error (err)
   boards.forEach(board => {
     if (!board.closed) {
       trello.getListsOnBoard(board.id, function(err, lists) {
-        lists.forEach((list) => {
+        if (err) throw new Error (err)
+        lists.forEach(list => {
           trello.getCardsOnList(list.id, function(err, cards) {
+            if (err) throw new Error (err)
             // Only return the lists we are interested in and skip over lists with no cards
             if ( listNameArray.includes(list.name) && cards.length > 0) {
               const boardLink = terminalLink(board.name, board.shortUrl)
               console.log(' ')
               console.log(`${chalk.bgGreen.black.bold(' ✔ ')}${chalk.bgWhite(` ${chalk.red(list.name)} ${chalk.black(`on`)} ${chalk.blue(boardLink)} `)}`)
               console.log(' ')
-              cards.map(card => {
-                const cardLink = terminalLink(card.name, card.shortUrl)
-                console.log(' ', cardLink)
-              })
+              cards.map(card => 
+                console.log(' ', terminalLink(card.name, card.shortUrl))
+              )
             }
           })
         })
